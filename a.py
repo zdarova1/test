810ad764927b70ed062e2de7ec6013cdbeed8414
zdarova1 <34815465+zdarova1@users.noreply.github.com>
2022-10-17 01:09:41 +0300

    Add files via upload

--- a.py
+++ a.py
@@ -3,6 +3,7 @@ from fileinput import filename
 from msilib.schema import Error
 import sqlite3
 from os import curdir
+from prettytable import PrettyTable
 
 
 def init_tables(cursor: sqlite3.Cursor, connection: sqlite3.Connection):
@@ -26,11 +27,18 @@ def init_data(cursor: sqlite3.Cursor, connection: sqlite3.Connection, file_name:
     command = "INSERT INTO " + file_name[:-4] + " VALUES ("
     for line in init_file:
         s = command + '\''+'\',\''.join(line.split(' '))[:-1]+'\')'
-        #print(s)
+        print(s)
         cursor.execute(s)
         connection.commit()
     
     print(f"Таблица {file_name[:-4]} проинициализорована!")
+
+
+def  my_selects():
+    str = input(f''' 
+          show TABLES - 1
+          show client's orders with  
+          ''')
 
 
 
@@ -40,24 +48,24 @@ try:
     cursor = sql_connection.cursor()
     init_tables(cursor, sql_connection)
     
-    #init_data(cursor, sql_connection, f"available_cars.txt")
-    #init_data(cursor, sql_connection, f"manufacturer.txt")
+    #init_data(cursor, sql_connection, f"available_cars.txt") раскоменть
+   # init_data(cursor, sql_connection, f"manufacturer.txt")
     #init_data(cursor, sql_connection, f"clients_orders.txt")
-    #init_data(cursor, sql_connection, f"sanction_countries.txt")
+    #init_data(cursor, sql_connection, f"sanction_countries.txt")раскоменть
     command = input("Введите SQL команду: ")
     while command != "close":
         cursor.execute(command)
         
         if command.split(' ')[0] == "SELECT":
-            headers = cursor.description
-            header_s = (' '.join(t for x in headers for t in x if type(' ') == type(t)))
-            #format_header_s = f''
-            print(header_s) 
+            headers = [t for x in cursor.description for t in x if type(' ') == type(t)] 
+            table = PrettyTable(headers)
+            #print(headers)
+            
             records = cursor.fetchall()
             for line in records:
-                print(' '.join([x for x in line]))
-                #print(line)
-                
+                table.add_row(([x for x in line]))
+                #print(([x for x in line]))
+            print(table)
         command = input("Введите SQL команду: ")
                 
         
